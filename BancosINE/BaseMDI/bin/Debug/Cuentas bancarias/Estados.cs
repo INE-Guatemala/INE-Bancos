412fdb797fb0e097245d499df0b247cbbec1659d
guicho0601 <guicholarg@hotmail.com>
2014-06-04 20:26:01 -0600

    Cambio al formulario estados

---
+++ BancosINE/BaseMDI/bin/Debug/Cuentas bancarias/Estados.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BancosINE
+{
+    public partial class Estados : BaseForm
+    {
+
+        private bool isLoad = false;
+        private DataGridView dGrid;
+
+        public Estados()
+        {
+            InitializeComponent();
+            this.Name ="Estados de cuenta";
+            this.addTitle(this.Name);
+            dGrid = customGridView1.dGrid;
+
+            fillCombo();
+            consultarSaldo(1);   
+        }
+
+        private void fillCombo()
+        {
+            comboBox1.DataSource = Funciones.dbConnect.consulta_ComboBox("select idcuenta,nombre from cuenta");
+            comboBox1.DisplayMember = "nombre";
+            comboBox1.ValueMember = "idcuenta";
+
+            cuentaEstado.DataSource = comboBox1.DataSource;
+            cuentaEstado.DisplayMember = comboBox1.DisplayMember;
+            cuentaEstado.ValueMember = comboBox1.ValueMember;
+            isLoad = true;
+        }
+
+        private void consultarSaldo(int idcuenta)
+        {
+            if (isLoad)
+            {
+                string query = "select nombre as 'Nombre de cuenta',numero as 'Numero de cuenta',concat('Q.',saldo) as 'Saldo' from cuenta where idcuenta = " + idcuenta.ToString();
+                dGrid.DataSource = Funciones.dbConnect.consulta_DataGridView(query);
+                //dGrid.Columns[0].Visible = false;
+            }
+            
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(isLoad) consultarSaldo(Convert.ToInt32(comboBox1.SelectedValue.ToString()));
+        }
+
+        private void buscarBtn_Click(object sender, EventArgs e)
+        {
+            string fechaInicio = dateTimePicker1.Value.ToString("yyyy-MM-dd");
+            string fechaFin = dateTimePicker2.Value.ToString("yyyy-MM-dd");
+            string idcuenta = cuentaEstado.SelectedValue.ToString();
+
+            string query = "select m.fecha as 'Fecha', m.referencia as 'Referencia', t.nombre as 'Tipo de transaccion', m.monto as 'Monto' ";
+            query += " from movimiento m, transaccion t where m.idtransaccion=t.idtransaccion and m.idcuenta = " + idcuenta;
+            query += " and m.fecha <=" + fechaFin + " and m.fecha >=" + fechaInicio;
+
+            DataGridView dGrid = customGridView2.dGrid;
+            dGrid.DataSource = Funciones.dbConnect.consulta_DataGridView(query);
+        }
+
+        private void cuentaEstado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ BancosINE/BaseMDI/bin/Debug/Cuentas bancarias/Estados.designer.cs
@@ -0,0 +1,237 @@
+﻿namespace BancosINE
+{
+    partial class Estados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.customGridView1 = new BancosINE.CustomGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.customGridView2 = new BancosINE.CustomGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cuentaEstado = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.buscarBtn = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(19, 54);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(989, 699);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.customGridView1);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(981, 670);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Saldos de cuenta";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(781, 268);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 39);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Imprimir";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // customGridView1
+            // 
+            this.customGridView1.Location = new System.Drawing.Point(9, 60);
+            this.customGridView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.customGridView1.Name = "customGridView1";
+            this.customGridView1.Size = new System.Drawing.Size(888, 203);
+            this.customGridView1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(9, 26);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(887, 24);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.tabPage2.Controls.Add(this.buscarBtn);
+            this.tabPage2.Controls.Add(this.dateTimePicker2);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.dateTimePicker1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.cuentaEstado);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.customGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(981, 670);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Estados de cuenta";
+            // 
+            // customGridView2
+            // 
+            this.customGridView2.Location = new System.Drawing.Point(7, 106);
+            this.customGridView2.Name = "customGridView2";
+            this.customGridView2.Size = new System.Drawing.Size(967, 557);
+            this.customGridView2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cuenta:";
+            // 
+            // cuentaEstado
+            // 
+            this.cuentaEstado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuentaEstado.FormattingEnabled = true;
+            this.cuentaEstado.Location = new System.Drawing.Point(101, 19);
+            this.cuentaEstado.Name = "cuentaEstado";
+            this.cuentaEstado.Size = new System.Drawing.Size(796, 28);
+            this.cuentaEstado.TabIndex = 2;
+            this.cuentaEstado.SelectedIndexChanged += new System.EventHandler(this.cuentaEstado_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "De fecha:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(101, 60);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(276, 26);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(497, 60);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(265, 26);
+            this.dateTimePicker2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(420, 60);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "A fecha:";
+            // 
+            // buscarBtn
+            // 
+            this.buscarBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(35)))), ((int)(((byte)(38)))));
+            this.buscarBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buscarBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buscarBtn.Location = new System.Drawing.Point(780, 60);
+            this.buscarBtn.Name = "buscarBtn";
+            this.buscarBtn.Size = new System.Drawing.Size(117, 28);
+            this.buscarBtn.TabIndex = 7;
+            this.buscarBtn.Text = "Buscar";
+            this.buscarBtn.UseVisualStyleBackColor = false;
+            this.buscarBtn.Click += new System.EventHandler(this.buscarBtn_Click);
+            // 
+            // Estados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 768);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Estados";
+            this.Text = "Estados";
+            this.Controls.SetChildIndex(this.tabControl1, 0);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private CustomGridView customGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cuentaEstado;
+        private System.Windows.Forms.Label label2;
+        private CustomGridView customGridView2;
+        private System.Windows.Forms.Button buscarBtn;
+    }
+}
